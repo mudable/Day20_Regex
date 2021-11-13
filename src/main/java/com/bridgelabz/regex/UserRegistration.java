b8f40c4f857f5b392c9d7d009fbdfceb9ac50e95
mudable <mudableshilpa@gmail.com>
2021-11-13 07:18:33 +0530

    Added : UC4 for valid mobile number using country code

--- src/main/java/com/bridgelabz/regex/UserRegistration.java
+++ src/main/java/com/bridgelabz/regex/UserRegistration.java
@@ -6,8 +6,8 @@ import java.util.regex.Pattern;
 public class UserRegistration {
 	/*
 	 * /*created method first name which take the name start from capital. done the
-	 * regex pattern for that. Added the same pattern for last name.
-	 * Used regex pattern gor valid gmail.
+	 * regex pattern for that. Added the same pattern for last name. Used regex
+	 * pattern for valid gmail.
 	 */
 	Scanner sc = new Scanner(System.in);
 
@@ -16,6 +16,7 @@ public class UserRegistration {
 		userRegistration.firstName();
 		userRegistration.lastName();
 		userRegistration.email();
+		userRegistration.contactNumber();
 	}
 
 //User's First Name
@@ -51,6 +52,18 @@ public class UserRegistration {
 			System.out.println("thank you");
 		} else {
 			System.out.println("please enter valid email ID ");
+		}
+	}
+
+	public void contactNumber() {
+		sc = new Scanner(System.in);
+		System.out.print("Enter contact number with country code :: ");
+		String ContactNumber = sc.nextLine();
+		boolean answer = Pattern.matches("91\\s[0-9]{10}", ContactNumber);
+		if (answer) {
+			System.out.println("Thank you");
+		} else {
+			System.out.println("enter valid contact number");
 		}
 	}
 }
