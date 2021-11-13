3bf513adc293b8331fc473bcee575e2449372426
mudable <mudableshilpa@gmail.com>
2021-11-13 07:10:25 +0530

    Added : UC3 to check the given email is valid or not

--- src/main/java/com/bridgelabz/regex/UserRegistration.java
+++ src/main/java/com/bridgelabz/regex/UserRegistration.java
@@ -6,20 +6,20 @@ import java.util.regex.Pattern;
 public class UserRegistration {
 	/*
 	 * /*created method first name which take the name start from capital. done the
-	 * regex pattern for that.
-	 * Added the same pattern for last name.
+	 * regex pattern for that. Added the same pattern for last name.
+	 * Used regex pattern gor valid gmail.
 	 */
+	Scanner sc = new Scanner(System.in);
+
 	public static void main(String[] args) {
 		UserRegistration userRegistration = new UserRegistration();
 		userRegistration.firstName();
 		userRegistration.lastName();
+		userRegistration.email();
 	}
-
-	Scanner sc;
 
 //User's First Name
 	public void firstName() {
-		sc = new Scanner(System.in);
 		System.out.print("Enter First Name :: ");
 		String FirstName = sc.next();
 		boolean answer = Pattern.matches("^[A-Z]{1}([a-z]*){3,}", FirstName);
@@ -32,7 +32,6 @@ public class UserRegistration {
 
 //User's Last Name 
 	public void lastName() {
-		sc = new Scanner(System.in);
 		System.out.print("Enter Last Name :: ");
 		String LastName = sc.next();
 		Boolean answer = Pattern.matches("^[A-Z]{1}([a-z]*){3,}", LastName);
@@ -40,6 +39,18 @@ public class UserRegistration {
 			System.out.println("thank you");
 		} else {
 			System.out.println("please enter valid last name ");
+		}
+	}
+
+	// User's Email Address
+	public void email() {
+		System.out.print("Enter email ID :: ");
+		String Email = sc.next();
+		Boolean answer = Pattern.matches("^[a-zA-Z0-9+_!#$%&’*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$", Email);
+		if (answer) {
+			System.out.println("thank you");
+		} else {
+			System.out.println("please enter valid email ID ");
 		}
 	}
 }
